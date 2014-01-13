8ebcb19575429c8c5583e6eedffc701faf598ebc
duhaitao <dht@dht.none.org>
2014-01-13 16:52:00 +0800

    01.go

--- 01.go
+++ 01.go
@@ -3,8 +3,36 @@ package main
 import (
 	"fmt"
 	"time"
+	"math/rand"
 )
 
 func main () {
 	fmt.Println (time.Now ().UnixNano ())
+	/// ticker := time.NewTicker (time.Second)
+	retChan := make (chan int)
+	timeChan := make (chan int)
+
+	go func (retChan *chan int) {
+		for {
+			time.Sleep (time.Millisecond * 100)
+			*retChan<- rand.Int ()
+		}
+	}(&retChan)
+
+	go func (timeChan *chan int) {
+		for {
+			time.Sleep (time.Millisecond * 800)
+			*timeChan<- 0
+		}
+	}(&timeChan)
+
+	var ret int
+	for {
+		select {
+			case <-timeChan:
+				fmt.Println ("---------------------------------")
+			case ret = <-retChan:
+				fmt.Println ("random: ", ret)
+		}
+	}
 }
