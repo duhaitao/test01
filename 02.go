f852f0327fb8e1364b66c271eb7d3273d34be11e
duhaitao <duhaitao1974@gmail.com>
2014-02-03 22:32:48 +0800

    mod 02.go

--- 02.go
+++ 02.go
@@ -6,8 +6,22 @@ import (
 
 func main () {
 	fmt.Println ("hello")
+	//var sum int
 
 	aaa := make ([]int, 10)
-	aaa[0] = 1
+	for i := 1; i <= 10; i++ {
+		aaa[i - 1] = i
+	}
+
+	ch := make (chan int)
+	go func (aaa []int, ch chan int) {
+		var sum int
+		for _, val := range aaa {
+			sum += val
+		}
+		ch<- sum
+	} (aaa, ch)
+	sum := <-ch
+	fmt.Println (sum)
 }
 
